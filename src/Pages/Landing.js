70c8c03f9094150af3ba919654a4261d38e23016
Nico Bellanich <nicolasbellanich@gmail.com>
2021-02-14 00:25:56 -0300

    Add my image

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,14 +1,13 @@
 import React from "react";
-
+import img from "../Images/me.jpg"
 const Landing = () => {
   return (
     <>
+      <img style={{height:"59vh",borderRadius:"30px"}}src={img} alt="hi"></img>
       <h1 style={{ fontWeight: "400" }}>
-        Hello I´m Nicolas and I´d like you to know me
+        Hello I´m Nicolas and I´d like you to know my work!
       </h1>
-      <h2 style={{ fontWeight: "400" }}>
-        click to continue
-      </h2>
+      <h2 style={{ fontWeight: "400" }}>click to continue</h2>
     </>
   );
 };
