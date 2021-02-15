c9e9c3d3e39cc3bc6755a9f9a0e201d5254e43fe
Nico Bellanich <nicolasbellanich@gmail.com>
2021-02-15 14:31:39 -0300

    Reorder Components

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,17 +1,9 @@
 import React from "react";
-import img from "../Images/Worlds.svg"
-import ApearAnimation from "../Components/Organisms/Animations/Apear"
+import LandingSection from "../Components/Templates/LandingSection"
 const Landing = () => {
   return (
     <>
-      <ApearAnimation delayTime={500} duration={2000}>
-        <img style={{height:"100vh",borderRadius:"30px"}}src={img} alt="hi"></img>
-        <ApearAnimation delayTime={1500} duration={2000}>
-          <h1 style={{ fontWeight: "400" , position:"absolute", top:"20px", left:"150px", width:"40%"}}>
-            Welcome to my world
-          </h1>
-        </ApearAnimation>
-      </ApearAnimation>
+      <LandingSection />
     </>
   );
 };
