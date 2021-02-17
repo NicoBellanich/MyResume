5a26c932f5bea9c937418fa5d741f35b30bfd3f0
Nico Bellanich <nicolasbellanich@gmail.com>
2021-02-17 01:13:01 -0300

    Add contact in linkeidn

--- src/Components/Templates/ContactInfoSection.js
+++ src/Components/Templates/ContactInfoSection.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ApearAnimation from "../../Components/Organisms/Animations/Apear";
 import styles from "./ContactInfoSection.module.css";
+import { FaLinkedin as LinkedInIcon } from 'react-icons/fa';
 const ContactInfoSection = () => {
   return (
     <div className={styles.container}>
       <ApearAnimation delayTime={0} duration={2000}>
-        <h1>hi</h1>
+        <h1 className={styles.title}>Contact</h1>
+        <p>
+            <a href="https://www.linkedin.com/in/nicolasdariobellanich70491/">Nico Bellanich</a>
+            
+          <LinkedInIcon />
+        </p>
       </ApearAnimation>
     </div>
   );
